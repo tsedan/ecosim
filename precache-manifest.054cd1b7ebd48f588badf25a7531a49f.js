548b554bd9314c499e36e8551ab7e7f360689363
Tomer Sedan <tomer.sedan@gmail.com>
2018-12-24 09:03:25 -0800

    Updates

---
+++ precache-manifest.054cd1b7ebd48f588badf25a7531a49f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/homepage/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c9f2de54d44bb9049b00",
+    "url": "/homepage/static/js/runtime~main.c9f2de54.js"
+  },
+  {
+    "revision": "65dda72ad2d7a37281a6",
+    "url": "/homepage/static/js/main.65dda72a.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/homepage/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "65dda72ad2d7a37281a6",
+    "url": "/homepage/static/css/main.981b6429.chunk.css"
+  },
+  {
+    "revision": "c61038a64210fd5ce88c4bc874ca5a47",
+    "url": "/homepage/index.html"
+  }
+];
