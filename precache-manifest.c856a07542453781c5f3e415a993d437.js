bea765b3221fb6bb79c2f47ac8247c4aed01660f
Tomer Sedan <tomer.sedan@gmail.com>
2018-12-23 21:23:11 -0800

    Updates

---
+++ precache-manifest.c856a07542453781c5f3e415a993d437.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "29b00ebcf93fda46ac9957fa4816eafd",
+    "url": "/commsim/static/media/Inconsolata.29b00ebc.ttf"
+  },
+  {
+    "revision": "c455ec93e9cc618ef18349f77cfa19c0",
+    "url": "/commsim/static/media/AdamCGPro.c455ec93.otf"
+  },
+  {
+    "revision": "c570df8ddc532fb8ceb3",
+    "url": "/commsim/static/js/runtime~main.c570df8d.js"
+  },
+  {
+    "revision": "b302c41581568a1ccfdc",
+    "url": "/commsim/static/js/main.b302c415.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/commsim/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "b302c41581568a1ccfdc",
+    "url": "/commsim/static/css/main.7c44ad77.chunk.css"
+  },
+  {
+    "revision": "df3282df185d23d652db294eee9e4609",
+    "url": "/commsim/index.html"
+  }
+];
