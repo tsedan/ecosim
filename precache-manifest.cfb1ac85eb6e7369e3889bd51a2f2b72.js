5c700f5ca7538549aef62120ee949cda505482a0
Tomer Sedan <tomer.sedan@gmail.com>
2018-12-24 09:22:21 -0800

    Updates

---
+++ precache-manifest.cfb1ac85eb6e7369e3889bd51a2f2b72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3a0cf3673fe98633f8c2",
+    "url": "/static/js/main.3a0cf367.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "3a0cf3673fe98633f8c2",
+    "url": "/static/css/main.c2bc3e57.chunk.css"
+  },
+  {
+    "revision": "7480fd5501d19cd188f684aa44eeca64",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/commsim/precache-manifest.976cf7acb821dfbccb6c8b5a9abe8544.js"
+  "/precache-manifest.cfb1ac85eb6e7369e3889bd51a2f2b72.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/commsim/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
